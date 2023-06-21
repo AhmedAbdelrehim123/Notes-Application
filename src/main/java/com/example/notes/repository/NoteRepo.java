b8c571050931da0e01c3f1dfe8f20a0e95423af6
AhmedAbdelrehim123 <100535026+AhmedAbdelrehim123@users.noreply.github.com>
2023-06-21 09:49:37 +0100

    Test for NoteController

--- src/main/java/com/example/notes/repository/NoteRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.notes.repository;
-
-import com.example.notes.model.Note;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NoteRepo
-        extends MongoRepository<Note, Integer> {
-}
